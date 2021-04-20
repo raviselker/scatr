5c1acd46ec4b49ecc0a1c7082ba9b676e62643e0
Ravi <selker.ravi@gmail.com>
2021-04-20 17:41:03 +0200

    scat: use patchwork instead of cowplot

--- R/scat.b.R
+++ R/scat.b.R
@@ -47,135 +47,131 @@ scatClass <- R6::R6Class(
             marg <- self$options$marg
             line <- self$options$line
             method <- ifelse(line == "linear", "lm", "auto")
+                    
+            p <- 
+                ggplot2::ggplot(data, ggplot2::aes(x=x, y=y, color=g, fill=g)) + 
+                ggplot2::geom_point(alpha=.8, size=2.5) + 
+                ggtheme +
+                ggplot2::labs(
+                    x=self$options$x, 
+                    y=self$options$y, 
+                    fill=self$options$group, 
+                    color=self$options$group
+                )
             
-            base::suppressMessages({
-                base::suppressWarnings({
-                    
-                    p <- 
-                        ggplot2::ggplot(
-                            data, ggplot2::aes(x=x, y=y, color=g, fill=g)
-                        ) + 
-                        ggplot2::geom_point(alpha=.8, size=2.5) + 
-                        ggtheme +
-                        ggplot2::labs(
-                            x=self$options$x, 
-                            y=self$options$y, 
-                            fill=self$options$group, 
-                            color=self$options$group
+            if (line != "none") {
+                p <- p + 
+                    ggplot2::geom_smooth(method = method, se = self$options$se)
+            }
+
+            colors <- NULL
+            if (is.null(self$options$group)) {
+                colors <- list(
+                    ggplot2::scale_color_manual(values=theme$color[1]),
+                    ggplot2::scale_fill_manual(values=theme$fill[2]), 
+                    ggplot2::scale_shape_manual(values=21)
+                )
+                p <- p + 
+                    ggplot2::theme(legend.position = "none") + colors
+            }
+            
+            if (marg == "dens") {
+                xdens <- 
+                    ggplot2::ggplot(data=data, ggplot2::aes(x=y, fill=g)) +
+                    ggplot2::geom_density(alpha=0.5, size=.2) + 
+                    ggtheme + 
+                    ggplot2::theme_void() +
+                    ggplot2::theme(legend.position = "none") +
+                    colors 
+                
+                ydens <- 
+                    ggplot2::ggplot(data=data, ggplot2::aes(x=y, fill=g)) +
+                    ggplot2::geom_density(alpha=0.5, size=.2) + 
+                    ggplot2::coord_flip() +
+                    ggtheme + 
+                    ggplot2::theme_void() +
+                    ggplot2::theme(legend.position = "none") +
+                    colors
+                
+                p <-
+                    xdens + patchwork::plot_spacer() + 
+                    p + ydens + 
+                    patchwork::plot_layout(
+                        ncol=2, 
+                        nrow=2, 
+                        widths = c(4, 1), 
+                        heights = c(1, 4),
+                        guides = "collect"
+                    ) & 
+                    ggplot2::theme(
+                        plot.background = ggplot2::element_rect(
+                            fill = "transparent", colour = NA
                         )
-                    
-                    if (line != "none") {
-                        p <- p + 
-                            ggplot2::geom_smooth(
-                                method = method, se = self$options$se
-                            )
-                    }
-        
-                    colors <- NULL
-                    if (is.null(self$options$group)) {
-                        colors <- list(
-                            ggplot2::scale_color_manual(values=theme$color[1]),
-                            ggplot2::scale_fill_manual(values=theme$fill[2]), 
-                            ggplot2::scale_shape_manual(values=21)
+                    )
+            } else if (marg == "box") {
+                themeBox <- ggplot2::theme_void() +
+
+                    ggplot2::theme(
+                        panel.grid.major = ggplot2::element_blank(),
+                        panel.grid.minor = ggplot2::element_blank(),
+                        strip.background = ggplot2::element_rect(
+                            fill="transparent", color=NA
+                        ),
+                        panel.background=ggplot2::element_rect(
+                            fill="transparent", color=NA
+                        ),
+                        legend.position = "none"
+                    )
+                
+                xdens <- ggplot2::ggplot() +
+                    ggplot2::geom_boxplot(
+                        data=data, 
+                        ggplot2::aes(x=g, y=x, fill=g, color=g), 
+                        position=ggplot2::position_dodge(0.8), 
+                        width=0.5, 
+                        alpha=0.9, 
+                        notch=TRUE
+                    ) + 
+                    ggplot2::coord_flip() +
+                    ggtheme + 
+                    themeBox + 
+                    colors
+                
+                ydens <- ggplot2::ggplot() +
+                    ggplot2::geom_boxplot(
+                        data=data, 
+                        ggplot2::aes(x=g, y=y, fill=g, color=g), 
+                        position=ggplot2::position_dodge(0.8),
+                        width=0.5, 
+                        alpha=0.9, 
+                        notch=TRUE
+                    ) + 
+                    ggtheme + 
+                    themeBox + 
+                    colors
+                
+                nLevels <- length(levels(data$g))
+                widths <- c(7, 1 + nLevels * 0.2)
+                heights <- c(1 + nLevels * 0.2, 7)
+                
+                p <-
+                    xdens + patchwork::plot_spacer() + 
+                    p + ydens + 
+                    patchwork::plot_layout(
+                        ncol = 2, 
+                        nrow = 2, 
+                        widths = widths, 
+                        heights = heights,
+                        guides = "collect"
+                    ) & 
+                    ggplot2::theme(
+                        plot.background = ggplot2::element_rect(
+                            fill = "transparent", colour = NA
                         )
-                        p <- p + 
-                            ggplot2::theme(legend.position = "none") + colors
-                    }
-                    
-                    if (marg == "dens") {
-                        xdens <- 
-                            cowplot::axis_canvas(p, axis="x") +
-                            ggridges::geom_ridgeline(
-                                data=data, 
-                                ggplot2::aes(x, y=0, height=..density.., fill=g), 
-                                stat="density", 
-                                alpha=0.5, 
-                                size=.2, 
-                                trim=FALSE
-                            ) + 
-                            ggtheme + 
-                            colors 
-                        
-                        ydens <- 
-                            cowplot::axis_canvas(p, axis="y") +
-                            ggridges::geom_vridgeline(
-                                data=data, 
-                                ggplot2::aes(x=0, y=y, width=..density.., fill=g), 
-                                stat="ydensity", 
-                                alpha=0.5, 
-                                size=.2, 
-                                trim=FALSE
-                            ) + 
-                            ggtheme + 
-                            colors
-                        
-                        p <- cowplot::insert_xaxis_grob(
-                            p, xdens, grid::unit(.2, "null"), position="top"
-                        )
-                        p <- cowplot::insert_yaxis_grob(
-                            p, ydens, grid::unit(.2, "null"), position="right"
-                        )
-                        
-                        p <- cowplot::ggdraw(p)    
-                    } else if (marg == "box") {
-                        themeBox <- ggplot2::theme(
-                            panel.grid.major = ggplot2::element_blank(),
-                            panel.grid.minor = ggplot2::element_blank(),
-                            strip.background = ggplot2::element_rect(
-                                fill="transparent", color=NA
-                            ),
-                            panel.background=ggplot2::element_rect(
-                                fill="transparent", color=NA
-                            )
-                        )
-                        
-                        xdens <- ggplot2::ggplot() +
-                            ggplot2::geom_boxplot(
-                                data=data, 
-                                ggplot2::aes(x=g, y=x, fill=g, color=g), 
-                                position=ggplot2::position_dodge(0.8), 
-                                width=0.5, 
-                                alpha=0.9, 
-                                notch=TRUE
-                            ) + 
-                            ggtheme + 
-                            themeBox + 
-                            colors +
-                            ggplot2::coord_flip()
-                        
-                        ydens <- ggplot2::ggplot() +
-                            ggplot2::geom_boxplot(
-                                data=data, 
-                                ggplot2::aes(x=g, y=y, fill=g, color=g), 
-                                position=ggplot2::position_dodge(0.8),
-                                width=0.5, 
-                                alpha=0.9, 
-                                notch=TRUE
-                            ) + 
-                            ggtheme + 
-                            themeBox + 
-                            colors
-                        
-                        nLevels <- length(levels(data$g))
-                        
-                        p <- cowplot::insert_xaxis_grob(
-                            p, 
-                            xdens, 
-                            grid::unit(.05 * nLevels, "null"), 
-                            position="top"
-                        )
-                        p <- cowplot::insert_yaxis_grob(
-                            p, 
-                            ydens, 
-                            grid::unit(.05 * nLevels, "null"), 
-                            position="right"
-                        )
-                        
-                        p <- cowplot::ggdraw(p)
-                    }
-                    return(p)    
-                })
-            })
+                    )
+            }
+            
+            return(p)
         },
         
         #### Helper functions ----
